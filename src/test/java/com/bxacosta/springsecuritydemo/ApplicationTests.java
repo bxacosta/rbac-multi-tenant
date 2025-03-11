b035b3830703b1a3d9448d83ba2e0b35e562c020
Bryan Acosta <bxacosta@hotmail.com>
2025-03-10 23:29:06 -0500

    chore: init base project

---
+++ src/test/java/com/bxacosta/springsecuritydemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bxacosta.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
